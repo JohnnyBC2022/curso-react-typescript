4533cd83044218a51a16765df6a1f1542b6aeaeb
Johnny <jonathanbc2012@gmail.com>
2024-11-12 13:57:28 +0100

    feat: 11.js added

---
+++ js/11.js
@@ -0,0 +1,13 @@
+// Destructuring de arrays
+
+const tecnologies = ["HTML", "CSS", "Javascript", "React.js", "Node.js"];
+
+const react = tecnologies[3];
+// Los extrae en función de la posición, así que ojo
+const [html, css, nodejs] = tecnologies
+
+console.log(nodejs)
+
+// imaginemos que solo queremos extraer el valor de la posición 4:
+const [, , ,reactjs] = tecnologies;
+console.log(reactjs)
