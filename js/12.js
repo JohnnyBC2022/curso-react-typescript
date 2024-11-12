4b8c3695c3f57b6da3d747c8db64c6be619c5542
Johnny <jonathanbc2012@gmail.com>
2024-11-12 14:02:18 +0100

    feat: 12.js added

---
+++ js/12.js
@@ -0,0 +1,14 @@
+const tecnologies = ["HTML", "CSS", "Javascript", "React.js", "Node.js"];
+
+/*
+Para evitar hacer: 
+console.log(tecnologies[0])
+console.log(tecnologies[1])
+console.log(tecnologies[2])
+console.log(tecnologies[3])
+console.log(tecnologies[4])
+*/
+
+for (let i = 0; i < tecnologies.length; i++) {
+  console.log(tecnologies[i]);
+}
