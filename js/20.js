8c3d9478080c1e7e3afc7329035517468e8cbea1
Johnny <jonathanbc2012@gmail.com>
2024-11-13 13:40:03 +0100

    feat: 20.js added

---
+++ js/20.js
@@ -0,0 +1,15 @@
+const number1 = 20;
+const number2 = "20";
+
+/*
+    == (Comparador no estricto)
+    === (Comparador estricto)
+*/
+console.log(typeof number1);
+console.log(typeof number2);
+
+if (number1 === number2) {
+  console.log("Son iguales");
+} else {
+  console.log("No son iguales");
+}
