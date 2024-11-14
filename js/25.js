0642237882743112b2f792da14eb1679a5514d1f
Johnny <jonathanbc2012@gmail.com>
2024-11-14 13:38:44 +0100

    feat: 25.js and funciones.js modified

--- js/25.js
+++ js/25.js
@@ -1,7 +1,12 @@
-import { sumar, restar } from "./funciones.js";
+import hola, { sumar, restar } from "./funciones.js";
 // Este import funciona porque en el HTML le estamos diciendo en el script que sea de tipo módulo.
 // Para evitar la colisión de nombres se le puede colocar un alias a la función que queremos importar.
 // import { sumar as funcionSumar } from "./funciones.js";
+
+// Cuando importamos una función que tiene el default, no es necesario poner el import entre llaves y podemos llamarlo como queramos. Va a funcionar porque el default es único en el archivo.
 const resultado = sumar(10, 20);
 
 console.log(resultado);
+
+const resultado2 = hola(100, 20);
+console.log(resultado2);
--- js/funciones.js
+++ js/funciones.js
@@ -1,10 +1,10 @@
 // Si utilizamos esta sintaxis, en el import debemos llamar a la función con el mismo nombre y entre llaves.
 export function sumar(n1, n2) {
-    return n1 + n2;
+  return n1 + n2;
 }
 
 export function restar(n1, n2) {
-    return n1 - n2;
+  return n1 - n2;
 }
 /*
 Como alternativa se puede hacer lo siguiente:
@@ -20,4 +20,25 @@ export {
     sumar,
     restar
 };
+*/
+
+export default function multiplicar(n1, n2) {
+  return n1 * n2;
+}
+
+/* solo puede haber un export default por archivo */
+
+// Vemos que funciona perfectamente con function declaration. También funciona con arrow function
+/*
+    export const sumar = (n1, n2) => n1 + n2;
+    export const restar = (n1, n2) => n1 - n2;
+ */
+
+/*
+Ojo con el default que cambia un poco la sintaxis. Hay que hacer lo siguiente:
+    const multiplicar = (n1, n2) => n1 * n2;
+    export default multiplicar;
+    Y normalmente se hace así por convenio, pero también funciona al revés:
+    export default mutliplicar;
+    const multiplicar = (n1, n2) => n1 * n2;
 */
