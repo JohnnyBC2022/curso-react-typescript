68c4f554cf3e16f357b673e38f1abd5df2dd15af
Johnny <jonathanbc2012@gmail.com>
2024-11-14 13:26:18 +0100

    feat: 25.js and funciones.js modified

--- js/25.js
+++ js/25.js
@@ -1,4 +1,7 @@
 import { sumar, restar } from "./funciones.js";
+// Este import funciona porque en el HTML le estamos diciendo en el script que sea de tipo módulo.
+// Para evitar la colisión de nombres se le puede colocar un alias a la función que queremos importar.
+// import { sumar as funcionSumar } from "./funciones.js";
 const resultado = sumar(10, 20);
 
 console.log(resultado);
--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,4 @@
+// Si utilizamos esta sintaxis, en el import debemos llamar a la función con el mismo nombre y entre llaves.
 export function sumar(n1, n2) {
     return n1 + n2;
 }
@@ -5,3 +6,18 @@ export function sumar(n1, n2) {
 export function restar(n1, n2) {
     return n1 - n2;
 }
+/*
+Como alternativa se puede hacer lo siguiente:
+function sumar(n1, n2) {
+    return n1 + n2;
+}
+
+function restar(n1, n2) {
+    return n1 - n2;
+}
+
+export {
+    sumar,
+    restar
+};
+*/
