c4fa7cbf782601b09a12cef4a17a8ceca7f20c61
Johnny <jonathanbc2012@gmail.com>
2024-11-15 11:42:33 +0100

    feat: 32.js added

---
+++ js/32.js
@@ -0,0 +1,19 @@
+// Eventos del DOM - Inputs
+
+const inputName = document.querySelector("#nombre");
+
+inputName.addEventListener("input", (e) => {
+  console.log(e.target.value);
+});
+
+const inputPassword = document.querySelector("#password");
+
+inputPassword.addEventListener("input", functionPassword());
+
+function functionPassword() {
+  inputPassword.type = "text";
+
+  setTimeout(() => {
+    inputPassword.type = "password";
+  }, 3000);
+}
