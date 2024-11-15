6ae34831ec044d4eb210327ef16db980e42c5db5
Johnny <jonathanbc2012@gmail.com>
2024-11-15 11:47:32 +0100

    feat: 33.js added

---
+++ js/33.js
@@ -0,0 +1,16 @@
+// Eventos de DOM - Submit
+
+const form = document.querySelector("#formulario");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = document.querySelector("#nombre").value;
+    const password = document.querySelector("#password").value;
+
+    if(name === "" || password === "") {
+        console.log("Todos los campos son obligatorios");
+    } else {
+        console.log("Iniciando sesión...");
+    }
+});
